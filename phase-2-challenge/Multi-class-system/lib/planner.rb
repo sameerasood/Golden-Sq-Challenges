7be3dcffb1965c5bc298d2efb9e27f6c32e92ba6
sameera sood <sameerasood@gmail.com>
2023-01-25 16:35:05 +0000

    Phase-2 multi class challenge done

--- phase-2-challenge/Multi-class-system/lib/planner.rb
+++ phase-2-challenge/Multi-class-system/lib/planner.rb
@@ -1,34 +1,54 @@
 class Planner
   def initialize
+    @diary_entry = []
+    @todo = [] 
 
   end
 
-  def add(entry)
-    #entry an instance of contents
+  def add_entry(entry)
+    @diary_entry.push(entry)
+  end
+
+  def add_todo(todo)
+    @todo.push(todo)
   end
 
   def display_entries
     #returns list of diary entries
+    return @diary_entry
   end
 
-  def todolist
-    #returns todolist
+  def get_todolist
+    return @todo
   end
 end
 
-class Contents
-  def initialize(todo, entry)
-
+class DiaryEntry
+  def initialize(entry, phone_numbers)
+    @entry = entry
+    @numbers = phone_numbers
+    @entries = []
   end
 
-  def diary_entry
-  #list of entries
+  def get_entrylist
+    @entries.push(@entry)
+    return @entries.join("")
   end
 
-
-  def chosen_entry(reading_time, wpm)
+  def chosen_entry(wpm, minutes)
   #returns entry on the basis of how much time they have and their reading speed(wpm)
-  end
+    readable_entries = @entries.filter do |entry|
+    (entry.count(" ") + 1) <= wpm * minutes
+    end
+    return readable_entries.first
+    end
+
 end
 
+class ToDo
+ def initialize(task)
+  @task = task
+ end
+
+end
 
--- phase-2-challenge/Multi-class-system/spec/planner_integration.rb
+++ phase-2-challenge/Multi-class-system/spec/planner_integration.rb
@@ -1,10 +1,52 @@
 require 'planner'
 
 RSpec.describe 'Planner and Contents integration' do
-  it 'adds diary entry and list of phone numbers' do
+
+  it 'displays 1 diary entry with a list of phone numbers' do
     task = Planner.new
-    entry_1 = Contents.new("Order Grocery", ["It is really colde today", "1234 56748, 98710"])
-    task.add(entry_1)
-    expect(task.display_entries).to eq ["It is really colde today", "1234 56748, 98710"]
+    entry_1 = DiaryEntry.new("It is really cold today", "1234 56748, 98710")
+    task.add_entry(entry_1)
+    expect(task.display_entries).to eq [entry_1]
+  end
+
+  it 'displays multiple diary entries with a list of phone numbers' do
+    task = Planner.new
+    entry_1 = DiaryEntry.new("It is really cold today", "1234 56748, 98710")
+    entry_2 = DiaryEntry.new("Hello how are you", "343526, 3746838, 83646")
+    task.add_entry(entry_1)
+    task.add_entry(entry_2)
+    expect(task.display_entries).to eq [entry_1, entry_2]
+  end
+
+  it 'adds task to the todo list' do
+    event = Planner.new
+    task = ToDo.new("Order Grocery")
+    event.add_todo(task)
+    expect(event.get_todolist).to eq [task]
+  end
+
+  it 'adds multiple tasks to the todo list' do
+    event = Planner.new
+    task_1 = ToDo.new("Order Grocery")
+    task_2 = ToDo.new("Cook Dinner")
+    task_3 = ToDo.new("Meditate")
+    event.add_todo(task_1)
+    event.add_todo(task_2)
+    event.add_todo(task_3)
+    expect(event.get_todolist).to eq [task_1, task_2, task_3]
+  end
+
+  it 'returns 1 entry' do
+    event = Planner.new
+    entry_1 = DiaryEntry.new("It is really cold today tomorrow", "1234 56748, 98710")
+    
+    expect(entry_1.get_entrylist).to eq "It is really cold today tomorrow"
+  end
+
+  it 'returns 1 entry out of two entries' do
+    event = Planner.new
+    entry_1 = DiaryEntry.new("It is really cold today tomorrow", "1234 56748, 98710")
+    entry_1 = DiaryEntry.new("It is really cold today tomorrow everyday", "1234 56748, 98710")
+    expect(entry_1.get_entrylist).to eq "It is really cold today tomorrow everyday"
   end
 end
