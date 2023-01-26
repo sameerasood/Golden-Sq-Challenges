14fa3220019b62806b93e34d74cccc6b06c4f428
sameera sood <sameerasood@gmail.com>
2023-01-26 14:20:41 +0000

    Phase-3 Challenge 1

--- phase-3/codebases/mocking_bites/spec/task_list_spec.rb
+++ phase-3/codebases/mocking_bites/spec/task_list_spec.rb
@@ -12,4 +12,35 @@ RSpec.describe TaskList do
   end
 
   # Unit test `#all` and `#all_complete?` behaviour
+  it "returns 1 task" do
+    task_list = TaskList.new
+    task_1 = double :faketask1
+    task_list.add(task_1)
+    expect(task_list.all).to eq [task_1]
+  end
+
+  it "returns 2 task2" do
+    task_list = TaskList.new
+    task_1 = double :faketask1
+    task_2 = double :faketask2
+    task_list.add(task_1)
+    task_list.add(task_2)
+    expect(task_list.all).to eq [task_1, task_2]
+  end
+
+  it "returns false" do
+    task_list = TaskList.new
+    task_1 = double :faketask1
+    expect(task_1).to receive(:complete?).and_return(false)
+    task_list.add(task_1)
+    expect(task_list.all_complete?).to eq(false)
+  end
+
+  it "returns task if task is given" do
+    task_list = TaskList.new
+    task_1 = double :faketask1
+    expect(task_1).to receive(:complete?).and_return(true)
+    task_list.add(task_1)
+    expect(task_list.all_complete?).to eq(true)
+  end
 end
